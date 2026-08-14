195f0ef5eb8d113a8a29e1438700b737d8370200
Primordial-Devv <primordial.devv@gmail.com>
2026-08-14 22:07:30 +0200

    CHORE - Author and GitHub infrastructure

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Siku Studio'
+description 'The official multicharacter system of the SIKU ecosystem — a modern, modular and immersive resource for creating, managing, and selecting player characters with seamless integration and a polished user experience.'
+version '0.0.1'
+
+name 'siku_multicharacter'
+
+lua54 'yes'
+
+shared_scripts {}
+
+server_scripts {}
+
+client_scripts {}
+
+ui_page ''
+
+files {}
+
+dependencies {}
