03a477fa354e615dd1fb7d96646b0a261c61a3ae
Primordial-Devv <primordial.devv@gmail.com>
2026-08-16 21:43:46 +0200

    CHORE - Remove NUI ready debug print

--- client/modules/nui/locale.lua
+++ client/modules/nui/locale.lua
@@ -16,7 +16,6 @@ local function loadTranslations()
 end
 
 RegisterNUICallback('siku_multicharacter:nui:ready', function(_, cb)
-  Siku.print.debug('NUI ready, sending locale')
   SendNUIMessage({
     action = 'siku_multicharacter:nui:setLocale',
     locale = {
